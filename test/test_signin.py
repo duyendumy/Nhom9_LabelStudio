06d8e62084e66ae5b5b5c22776763725900e9a74
DuyenDu <dumyduyen02@gmail.com>
2023-04-14 14:07:44 +0700

    Update pytest and allure

--- test/test_signin.py
+++ test/test_signin.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
+import allure
 
 class TestSingIn():
     @pytest.fixture()
@@ -9,80 +10,113 @@ class TestSingIn():
         driver = webdriver.Chrome(executable_path="D:/Cong cu va Moi truong phat trien phan mem/chromedriver/chromdriver.exe")
         driver.implicitly_wait(15)
         driver.maximize_window
+        driver.get("http://localhost:8080")
         yield
         driver.close()
         driver.quit()
         
+    @allure.description("Validate Label Studio with valid login credentials")  
+    @allure.severity(severity_level = "CRITICAL")  
     def test_login_by_valid_user(self, test_setup):
-        driver.get("http://localhost:8080")
-        email = driver.find_element(By.ID,'email')
-        # Send your email
-        email.send_keys("labelstudio09@gmail.com")
-        password = driver.find_element(By.ID,'password')
-        # Send your password
-        password.send_keys("1234asdfASDF")
-        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-        login_button.click()
-    
-    def test_login_by_incorrect_password(self, test_setup):
-        driver.get("http://localhost:8080")
-        email = driver.find_element(By.ID,'email')
-        # Send your email
-        email.send_keys("labelstudio09@gmail.com")
-        password = driver.find_element(By.ID,'password')
-        # Send your incorrect password
-        password.send_keys("incorrectpassword")
-        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-        login_button.click()
-        error = driver.find_element(By.CLASS_NAME,"error")
-        text = error.text
-        if text == "The email and password you entered don't match.":
-            print("Pass")
+        email_data = "labelstudio09@gmail.com"
+        password_data = "1234asdfASDF" 
+        with allure.step("Entering a valid email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
             
-    def test_login_by_incorrect_email(self, test_setup):
-        driver.get("http://localhost:8080")
-        email = driver.find_element(By.ID,'email')
-        # Send your incorrect email
-        email.send_keys("incorrectemail")
-        password = driver.find_element(By.ID,'password')
-        # Send your incorrect password
-        password.send_keys("1234asdfASDF")
+        with allure.step("Entering a valid password " + password_data):
+            password = driver.find_element(By.ID,'password')
+            password.send_keys(password_data)
+            
         login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
         login_button.click()
-        error = driver.find_element(By.CLASS_NAME,"error")
-        text = error.text
-        if text == "The email and password you entered don't match.":
-            print("Pass")
+        assert "projects" in driver.current_url
         
-    def test_login_by_invalid_email(self, test_setup):
-        driver.get("http://localhost:8080")
-        email = driver.find_element(By.ID,'email')
-        # Send your invalid email
-        email.send_keys("invalidemail@gmail.com")
-        password = driver.find_element(By.ID,'password')
-        # Send your invalid password
-        password .send_keys("1234")
-        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-        login_button.click()
-        error = driver.find_element(By.CLASS_NAME,"error")
-        text = error.text
-        if text == "The email and password you entered don't match.":
-            print("Pass")
     
-    def test_not_fill_field_login(self, test_setup):
-        driver.get("http://localhost:8080")
-        email = driver.find_element(By.ID,'email')
-        # Send your invalid email
-        email.send_keys("")
-        password = driver.find_element(By.ID,'password')
-        # Send your invalid password
-        password.send_keys("")
+    @allure.description("Validate Label Studio with invalid password")  
+    @allure.severity(severity_level = "NORMAL")  
+    def test_login_by_incorrect_password(self, test_setup):
+        email_data = "labelstudio09@gmail.com"
+        password_data = "incorrectpassword" 
+        with allure.step("Entering a valid email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
+            
+        with allure.step("Entering a invalid password " + password_data):
+            password = driver.find_element(By.ID,'password')
+            password.send_keys( password_data)
+            
+        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
+        login_button.click()
+        error = driver.find_element(By.CLASS_NAME,"error")
+        text = error.text     
+        if text == "The email and password you entered don't match.":
+                allure.attach(driver.get_screenshot_as_png(),
+                              name="Invalid Credentials", attachment_type=allure.attachment_type.PNG)
+            
+            
+    @allure.description("Validate Label Studio with invalid email")  
+    @allure.severity(severity_level = "NORMAL")          
+    def test_login_by_incorrect_email(self, test_setup):
+        email_data = "invalidemail@gmail.com"
+        password_data = "1234asdfASDF" 
+        with allure.step("Entering a invalid email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
+        
+        with allure.step("Entering a valid password " + password_data):
+            password = driver.find_element(By.ID,'password')
+            password.send_keys(password_data)
+            
         login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
         login_button.click()
         error = driver.find_element(By.CLASS_NAME,"error")
         text = error.text
         if text == "The email and password you entered don't match.":
-            print("Pass")
-        
-
- 
+           allure.attach(driver.get_screenshot_as_png(),
+                              name="Invalid Credentials", attachment_type=allure.attachment_type.PNG)
+            
+    @allure.description("Validate Label Studio with invalid login credentials")  
+    @allure.severity(severity_level = "NORMAL")      
+    def test_login_by_invalid_email(self, test_setup):
+        email_data = "invalidemail@gmail.com"
+        password_data = "1234" 
+        with allure.step("Entering a invalid email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
+            
+        with allure.step("Entering a invalid password " + password_data):
+            password = driver.find_element(By.ID,'password')   
+            password.send_keys(password_data)
+            
+        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
+        login_button.click()
+        error = driver.find_element(By.CLASS_NAME,"error")
+        text = error.text
+        if text == "The email and password you entered don't match.":
+            allure.attach(driver.get_screenshot_as_png(),
+                              name="Invalid Credentials", attachment_type=allure.attachment_type.PNG)
+    
+    @allure.description("Validate Label Studio with not fill field login")  
+    @allure.severity(severity_level = "NORMAL")  
+    def test_not_fill_field_login(self, test_setup):
+        email_data = ""
+        password_data = "" 
+        with allure.step("Not fill email"):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
+            
+        with allure.step("Not fill password"):
+            password = driver.find_element(By.ID,'password')
+            password.send_keys(password_data)
+            
+        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
+        login_button.click()
+        error = driver.find_element(By.CLASS_NAME,"error")
+        text = error.text
+        if text == "The email and password you entered don't match.":
+             allure.attach(driver.get_screenshot_as_png(),
+                              name="Invalid Credentials", attachment_type=allure.attachment_type.PNG)
+    
+     
+    
--- test/test_signup.py
+++ test/test_signup.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import pytest
+import allure
 
 class TestSingUp():
     @pytest.fixture()
@@ -15,33 +16,49 @@ class TestSingUp():
         driver.close()
         driver.quit()
     
+            
+    
+    @allure.description("Sign up Label Studio by existing account")  
+    @allure.severity(severity_level = "NORMAL")  
     def test_signup_by_existing_account(self, test_setup):
         signupLabels = WebDriverWait(driver, timeout=20).until(lambda d: d.find_elements(By.TAG_NAME,"a"))
         signupLabels[0].click()
-        email = driver.find_element(By.ID,'email')
-        # Send existing email
-        email.send_keys("labelstudio09@gmail.com")
-        password = driver.find_element(By.ID,'password')
-        # Send existing password
-        password .send_keys("1234asdfASDF")
+        email_data = "labelstudio09@gmail.com"
+        password_data = "1234asdfASDF"
+        with allure.step("Entering a existing email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys("labelstudio09@gmail.com")
+            
+        with allure.step("Entering a existing password " + password_data):
+            password = driver.find_element(By.ID,'password')
+            password .send_keys("1234asdfASDF")
+            
         signup_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
         signup_button.click()
         driver.implicitly_wait(60)
         # Check alert error
         if len(driver.find_elements(By.CLASS_NAME, "field_errors")) != 0:
-            print("Alert error 'User with this email already exists'")
+           allure.attach(driver.get_screenshot_as_png(),
+                              name="Invalid Authorization", attachment_type=allure.attachment_type.PNG)
     
+    @allure.description("Sign up Label Studio with not fill field login")  
+    @allure.severity(severity_level = "NORMAL")  
     def test_not_fill_out_sign_up(self, test_setup):
         signupLabels = WebDriverWait(driver, timeout=20).until(lambda d: d.find_elements(By.TAG_NAME,"a"))
         signupLabels[0].click()
-        email = driver.find_element(By.ID,'email')
-        email.send_keys("")
-        password = driver.find_element(By.ID,'password')
-        password .send_keys("")
+        
+        with allure.step("Not fill email"):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys("")
+            
+        with allure.step("Not fill password"):
+            password = driver.find_element(By.ID,'password')
+            password .send_keys("")
         signup_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
         signup_button.click()
         driver.implicitly_wait(60)
         # Check alert error
         if len(driver.find_elements(By.CLASS_NAME, "field_errors")) == 2:
-            print("Alert error 'Invalid email'")
-            print("Alert error 'Please enter a password 8-12 characters in length'")
+            allure.attach(driver.get_screenshot_as_png(),
+                              name="Invalid Authorization", attachment_type=allure.attachment_type.PNG)
+    
