d7179e7569f5492d6201b0219be6b2a994ec8ce7
DuyenDu <dumyduyen02@gmail.com>
2023-04-15 22:53:03 +0700

    Update test_invite

--- test/test_invite.py
+++ test/test_invite.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import pytest
+import allure
 
 class TestInvite():
     @pytest.fixture()
@@ -15,37 +16,49 @@ class TestInvite():
         driver.close()
         driver.quit()
         
+    @allure.description("Get token to invite member")  
+    @allure.severity(severity_level = "NORMAL")      
     def test_get_invite_link(self, test_setup):
-        email = driver.find_element(By.ID,'email')
-        # Send your email
-        email.send_keys("labelstudio09@gmail.com")
-        password = driver.find_element(By.ID,'password')
-        # Send your password
-        password .send_keys("1234asdfASDF")
+        email_data = "labelstudio09@gmail.com"
+        password_data = "1234asdfASDF"
+        with allure.step("Entering a valid email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
+            
+        with allure.step("Entering a valid password " + password_data):
+            password = driver.find_element(By.ID,'password')
+            password .send_keys("1234asdfASDF")
+            
         login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
         login_button.click()
-        
         hamburger = driver.find_element(By.CLASS_NAME,'ls-hamburger_animated')
         hamburger.click()
         menu_item = driver.find_elements(By.CLASS_NAME,'ls-main-menu__item')
         print(len(menu_item))
         for i in menu_item:
-           if(i.text == "Organization"):
-               i.click()
-               break
+            if(i.text == "Organization"):
+                i.click()
+                break
         add_people_btn = driver.find_element(By.CLASS_NAME,"ls-button_withIcon")
         add_people_btn.click()
         token = driver.find_element(By.CLASS_NAME,"ls-input")
-        link = token.get_attribute("value")
-        print("Token: " + link)
-    
+        with allure.step("Copy token"):
+            link = token.get_attribute("value")
+            print("Token: " + link)
+        
     def test_reset_invite_link(self, test_setup):
-        email = driver.find_element(By.ID,'email')
-        # Send your email
-        email.send_keys("labelstudio09@gmail.com")
-        password = driver.find_element(By.ID,'password')
-        # Send your password
-        password.send_keys("1234asdfASDF")
+        email_data = "labelstudio09@gmail.com"
+        password_data = "1234asdfASDF"
+        with allure.step("Entering a valid email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
+            
+        with allure.step("Entering a valid password " + password_data):
+            password = driver.find_element(By.ID,'password')
+            password .send_keys("1234asdfASDF")
+            
+        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
+        login_button.click()
         login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
         login_button.click()
         
@@ -58,6 +71,7 @@ class TestInvite():
                break
         add_people_btn = driver.find_element(By.CLASS_NAME,"ls-button_withIcon")
         add_people_btn.click()
+    
         reset_link_btn = driver.find_element(By.CLASS_NAME,"ls-button_look_")
         reset_link_btn.click()
         token = driver.find_element(By.CLASS_NAME,"ls-input")
