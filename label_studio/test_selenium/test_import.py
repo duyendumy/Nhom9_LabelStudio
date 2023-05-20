320ac7bc0c9653d1bf0095406ea1fce9a047671d
DuyenDu <dumyduyen02@gmail.com>
2023-05-20 08:53:47 +0700

    Refactor test_import

--- label_studio/test_selenium/test_import.py
+++ label_studio/test_selenium/test_import.py
@@ -17,29 +17,33 @@ class TestImportData():
     def test_setup(self):
         global driver
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-       # driver = webdriver.Chrome(executable_path="D:/Cong cu va Moi truong phat trien phan mem/chromedriver/chromdriver.exe")
         driver.implicitly_wait(15)
         driver.maximize_window
-        driver.get("http://localhost:8080")
+        # driver.get("http://localhost:8080")
         driver.get(constant.url)
         yield
         driver.close()
         driver.quit()
+        
+    @pytest.fixture()   
+    def test_login(self, test_setup):
+        email_data = "labelstudio09@gmail.com"
+        password_data = "1234asdfASDF"
+        with allure.step("Entering a valid email " + email_data):
+            email = driver.find_element(By.ID,'email')
+            email.send_keys(email_data)
+            
+        with allure.step("Entering a valid password " + password_data):
+            password = driver.find_element(By.ID,'password')
+            password .send_keys("1234asdfASDF")
+            
+        login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
+        login_button.click()
              
     
     @allure.description("Import data")  
     @allure.severity(severity_level = "CRITICAL") 
-    def test_import_dataset_url(self, test_setup):
-        email_data = "labelstudio09@gmail.com"
-        password_data = "1234asdfASDF" 
-        with allure.step("Sign in Label Studio"):
-            email = driver.find_element(By.ID,'email')
-            email.send_keys(email_data)
-            password = driver.find_element(By.ID,'password')
-            password.send_keys(password_data)                   
-            login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-            login_button.click()
-            
+    def test_import_dataset_url(self, test_login):     
         with allure.step("Select project to import"):
             list_project = driver.find_elements(By.CLASS_NAME, "ls-projects-page__link")
             list_project[0].click()
@@ -61,17 +65,7 @@ class TestImportData():
         
     @allure.description("Import image")  
     @allure.severity(severity_level = "CRITICAL") 
-    def test_import_upload_image(self, test_setup):
-        email_data = "labelstudio09@gmail.com"
-        password_data = "1234asdfASDF" 
-        with allure.step("Sign in Label Studio"):
-            email = driver.find_element(By.ID,'email')
-            email.send_keys(email_data)
-            password = driver.find_element(By.ID,'password')
-            password.send_keys(password_data)                   
-            login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-            login_button.click()
-            
+    def test_import_upload_image(self, test_login):            
         with allure.step("Select project to import"):
             list_project = driver.find_elements(By.CLASS_NAME, "ls-projects-page__link")
             list_project[0].click()
@@ -93,17 +87,7 @@ class TestImportData():
         
     @allure.description("Import json")  
     @allure.severity(severity_level = "CRITICAL") 
-    def test_import_upload_json(self, test_setup):
-        email_data = "labelstudio09@gmail.com"
-        password_data = "1234asdfASDF" 
-        with allure.step("Sign in Label Studio"):
-            email = driver.find_element(By.ID,'email')
-            email.send_keys(email_data)
-            password = driver.find_element(By.ID,'password')
-            password.send_keys(password_data)                   
-            login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-            login_button.click()
-            
+    def test_import_upload_json(self, test_login):
         with allure.step("Select project to import"):
             list_project = driver.find_elements(By.CLASS_NAME, "ls-projects-page__link")
             list_project[0].click()
@@ -125,17 +109,7 @@ class TestImportData():
     
     @allure.description("Import text")  
     @allure.severity(severity_level = "CRITICAL") 
-    def test_import_upload_text(self, test_setup):
-        email_data = "labelstudio09@gmail.com"
-        password_data = "1234asdfASDF" 
-        with allure.step("Sign in Label Studio"):
-            email = driver.find_element(By.ID,'email')
-            email.send_keys(email_data)
-            password = driver.find_element(By.ID,'password')
-            password.send_keys(password_data)                   
-            login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-            login_button.click()
-            
+    def test_import_upload_text(self, test_login):            
         with allure.step("Select project to import"):
             list_project = driver.find_elements(By.CLASS_NAME, "ls-projects-page__link")
             list_project[0].click()
@@ -157,17 +131,7 @@ class TestImportData():
             
     @allure.description("Import video")  
     @allure.severity(severity_level = "CRITICAL") 
-    def test_import_upload_text(self, test_setup):
-        email_data = "labelstudio09@gmail.com"
-        password_data = "1234asdfASDF" 
-        with allure.step("Sign in Label Studio"):
-            email = driver.find_element(By.ID,'email')
-            email.send_keys(email_data)
-            password = driver.find_element(By.ID,'password')
-            password.send_keys(password_data)                   
-            login_button = driver.find_element(By.CLASS_NAME,'ls-button_look_primary')
-            login_button.click()
-            
+    def test_import_upload_text(self, test_login):            
         with allure.step("Select project to import"):
             list_project = driver.find_elements(By.CLASS_NAME, "ls-projects-page__link")
             list_project[0].click()
