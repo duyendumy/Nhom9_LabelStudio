3b4cf3658503f5bb79783da435ca275612a81185
DuyenDu <dumyduyen02@gmail.com>
2023-05-23 10:44:22 +0700

    Add test_logout.py

--- label_studio/test_selenium/test_logout.py
+++ label_studio/test_selenium/test_logout.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 import allure
 import constant
@@ -10,11 +12,10 @@ class TestLogOut():
     def test_setup(self):
         global driver
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-       # driver = webdriver.Chrome(executable_path="D:/Cong cu va Moi truong phat trien phan mem/chromedriver/chromdriver.exe")
         driver.implicitly_wait(15)
         driver.maximize_window
-        # driver.get("http://localhost:8080")
-        driver.get(constant.url)
+        driver.get("http://localhost:8080")
+        # driver.get(constant.url)
         yield
         driver.close()
         driver.quit()
